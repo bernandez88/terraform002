56db123c27d52b67e056cb1cbf6da2669dd6389d
Byron Hernandez <bernandez88@gmail.com>
2021-12-02 21:37:59 -0600

    Fixing wrong character bug

--- local.tf
+++ local.tf
@@ -7,7 +7,7 @@ locals {
     resource_group_name_logic = format("rg_%s_%s_%s", substr(lower(replace(local.applicationname, " ", "")), 0, 15), var.location, random_string.suffix.result)
     resource_group_name   = var.resource_group_name != "" ? var.resource_group_name : local.resource_group_name_logic
 
-    app_service_name_logic = substr(format("appservice$s%s", substr(lower(replace(local.applicationname, " ","")), 0, 15),random_string.suffix.result), 0, 24)
+    app_service_name_logic = substr(format("appservice%s%s", substr(lower(replace(local.applicationname, " ","")), 0, 15),random_string.suffix.result), 0, 24)
     app_service_name = var.app_service_name != "" ? var.app_service_name : local.app_service_name_logic
 
     storage_account_name = "shareda1b2c3d4sa"
